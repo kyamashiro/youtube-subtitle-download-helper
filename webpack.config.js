f2beec55b63d4f04ec8d04c82e87735ec78298ff
kyamashiro <yamashiro@rrr-z.co.jp>
2019-07-12 13:05:36 +0900

    add start script

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+module.exports = {
+    // モジュールバンドルを行う起点となるファイルの指定
+    // 指定できる値としては、ファイル名の文字列や、それを並べた配列やオブジェクト
+    // 下記はオブジェクトとして指定した例 
+    entry: {
+        bundle: './src/app.ts'
+    },  
+    output: {
+        // モジュールバンドルを行った結果を出力する場所やファイル名の指定
+        // "__dirname"はこのファイルが存在するディレクトリを表すnode.jsで定義済みの定数
+        path: path.join(__dirname,'dist'),
+        filename: '[name].js'  // [name]はentryで記述した名前(この例ではbundle）が入る
+    },
+    // モジュールとして扱いたいファイルの拡張子を指定する
+    // 例えば「import Foo from './foo'」という記述に対して"foo.ts"という名前のファイルをモジュールとして探す
+    // デフォルトは['.js', '.json']
+    resolve: {
+        extensions:['.ts','.js']
+    },
+    devServer: {
+        // webpack-dev-serverの公開フォルダ
+        contentBase: path.join(__dirname,'dist')
+    },
+    // モジュールに適用するルールの設定（ここではローダーの設定を行う事が多い）
+    module: {
+        rules: [
+            {
+                // 拡張子が.tsで終わるファイルに対して、TypeScriptコンパイラを適用する
+                test:/\.ts$/,loader:'ts-loader'
+            }
+        ]
+    }
+}
