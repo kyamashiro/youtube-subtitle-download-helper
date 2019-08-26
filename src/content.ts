775987fe18e70c3d73379d0cbf104c99ce78ae9d
kyamashiro <kyamashiro73@gmail.com>
2019-08-27 08:02:24 +0900

    prettier code format

--- src/content.ts
+++ src/content.ts
@@ -1,36 +1,39 @@
-import VideoInfo from "./videoinfo";
-import Url from "./url";
+import VideoInfo from './videoinfo';
+import Url from './url';
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    const videoId = new Url(document.URL).getParam('v')
-    getRequest(`https://youtube.com/get_video_info?video_id=${videoId}`)
-        .then(response => {
-            const videoInfo = new VideoInfo(response)
-            sendResponse({ "captions": videoInfo.getCaptionsData(), "videoId": videoId, "title": videoInfo.getVideoTitle(), "error": null })
-        })
-        .catch(error => {
-            console.log(error);
-            sendResponse({ "error": error })
-        });
-    return true;
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  const videoId = new Url(document.URL).getParam('v');
+  getRequest(`https://youtube.com/get_video_info?video_id=${videoId}`)
+    .then(response => {
+      const videoInfo = new VideoInfo(response);
+      sendResponse({
+        captions: videoInfo.getCaptionsData(),
+        videoId: videoId,
+        title: videoInfo.getVideoTitle(),
+        error: null
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      sendResponse({ error: error });
+    });
+  return true;
 });
 
 function getRequest(url: string): Promise<any> {
-    return new Promise<any>(
-        function (resolve, reject) {
-            const request = new XMLHttpRequest();
-            request.onload = function () {
-                if (this.status === 200) {
-                    resolve(this.response);
-                } else {
-                    reject(new Error(this.statusText));
-                }
-            };
-            request.onerror = function () {
-                reject(new Error('XMLHttpRequest Error: ' + this.statusText));
-            };
-            request.open('GET', url);
-            request.send();
-        });
+  return new Promise<any>(function(resolve, reject) {
+    const request = new XMLHttpRequest();
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(this.response);
+      } else {
+        reject(new Error(this.statusText));
+      }
+    };
+    request.onerror = function() {
+      reject(new Error('XMLHttpRequest Error: ' + this.statusText));
+    };
+    request.open('GET', url);
+    request.send();
+  });
 }
-
--- src/videoinfo.ts
+++ src/videoinfo.ts
@@ -1,26 +1,30 @@
 export default class VideoInfo {
-    constructor(private videoInfoResponse: any) { }
+  constructor(private videoInfoResponse: any) {}
 
-    private videoInfoParse(): any {
-        const decodedData = this.parseQuery(this.videoInfoResponse)
-        return JSON.parse(decodedData.player_response)
-    }
+  private videoInfoParse(): any {
+    const decodedData = this.parseQuery(this.videoInfoResponse);
+    return JSON.parse(decodedData.player_response);
+  }
 
-    public getCaptionsData() {
-        return this.videoInfoParse().captions.playerCaptionsTracklistRenderer.captionTracks
-    }
+  public getCaptionsData() {
+    return this.videoInfoParse().captions.playerCaptionsTracklistRenderer
+      .captionTracks;
+  }
 
-    public getVideoTitle() {
-        return this.videoInfoParse().videoDetails.title.replace(/[\+|]/g, " ")
-    }
+  public getVideoTitle() {
+    return this.videoInfoParse().videoDetails.title.replace(/[\+|]/g, ' ');
+  }
 
-    private parseQuery(queryString: string) {
-        let query: { [key: string]: any } = {};
-        var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-        for (var i = 0; i < pairs.length; i++) {
-            var pair = pairs[i].split('=');
-            query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
-        }
-        return query;
+  private parseQuery(queryString: string) {
+    let query: { [key: string]: any } = {};
+    var pairs = (queryString[0] === '?'
+      ? queryString.substr(1)
+      : queryString
+    ).split('&');
+    for (var i = 0; i < pairs.length; i++) {
+      var pair = pairs[i].split('=');
+      query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
     }
+    return query;
+  }
 }
