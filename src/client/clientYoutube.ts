6d3b4a456f17bc0d52b8c87aa851200bb2d8731b
Kazuki Yamashiro <kyamashiro73@gmail.com>
2025-07-20 12:11:04 +0900

    wip

--- src/client/clientYoutube.ts
+++ src/client/clientYoutube.ts
@@ -1,9 +1,14 @@
+import type {
+  InnerTubeConfig,
+  PlayerData,
+} from "../parser/videoPageHtmlParser.ts";
+
 export class ClientYoutube {
   /**
-   * Get a list of translations by language
+   * Get HTML content of YouTube video page
    * @param videoId
    */
-  async getVideoInformation(videoId: string): Promise<string> {
+  async getVideoPageHtml(videoId: string): Promise<string> {
     const response = await fetch(`https://www.youtube.com/watch?v=${videoId}`);
     if (!response.ok) {
       throw new Error(response.statusText);
@@ -17,5 +22,34 @@ export class ClientYoutube {
       throw new Error(response.statusText);
     }
     return response.text();
+  }
+
+  async getPlayerData(
+    videoId: string,
+    innerTubeConfig: InnerTubeConfig,
+  ): Promise<PlayerData> {
+    const requestBody = {
+      context: {
+        client: {
+          clientName: innerTubeConfig.clientName,
+          clientVersion: innerTubeConfig.clientVersion,
+        },
+      },
+      videoId: videoId,
+    };
+
+    const response = await fetch("https://www.youtube.com/youtubei/v1/player", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!response.ok) {
+      throw new Error(`INNERTUBE API request failed: ${response.statusText}`);
+    }
+
+    return response.json();
   }
 }
